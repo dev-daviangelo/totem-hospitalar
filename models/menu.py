502b191c8ab70f38c75f39db5f3272809072676d
Davi Ângelo <davianp651@gmail.com>
2026-03-05 21:37:45 -0300

    Criado menu

---
+++ models/menu.py
@@ -0,0 +1,12 @@
+class Menu:
+
+    def mostrar_menu(self):
+
+        print("===MENU DE SERVIÇOS===")
+        print("1. Consulta")
+        print("2. Exame")
+        print("3. Emergência")
+
+        opcao = input("Escolha a opção: ")
+
+        return opcao
